474be974ac83ac11ddf313ceceacb831ff583f04
RootSquirrels <romain.bailleul@protonmail.com>
2026-01-26 10:51:10 +0100

    Update rds_instances_optimizations.py

--- checks/aws/rds_instances_optimizations.py
+++ checks/aws/rds_instances_optimizations.py
@@ -31,7 +31,17 @@ from typing import Any, Dict, Iterable, Iterator, List, Optional, Sequence, Tupl
 
 from botocore.exceptions import ClientError
 
-from checks.aws._common import normalize_tags
+from checks.aws._common import (
+    build_scope,
+    AwsAccountContext,
+    gb_from_bytes,
+    is_suppressed,
+    money,
+    now_utc,
+    safe_float,
+    safe_region_from_client,
+    utc,
+)
 from checks.registry import register_checker
 from contracts.finops_checker_pattern import FindingDraft, Scope, Severity
 
